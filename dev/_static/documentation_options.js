4ebe2ee1a640a00470d5de8167018ad299e33754
Kira Evans <contact@kne42.me>
2020-09-16 04:05:08 +0000

    update dev docs

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6.2.4.dev1+g2c472b2',
+    VERSION: '1.0.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
