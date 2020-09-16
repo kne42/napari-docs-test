9b7868c7ead42f951839df3a5d1e257d668d782c
Kira Evans <contact@kne42.me>
2020-09-16 17:35:33 +0000

    update dev docs autogenerated from kne42/napari@616a6549384d5c0020df95cd74298c7e6780f699

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.1.dev1+g64b1022',
+    VERSION: '1.0.3.dev1+g616a654',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
